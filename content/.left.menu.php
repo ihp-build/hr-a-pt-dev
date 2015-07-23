67b2cf0bf518914b0a6b4e0ef7d76dcd20542c7b
ihptru <ihptru@gmail.com>
2015-07-23 13:31:07 +0300

    first commit

---
+++ content/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Статьи", 
+		"/content/articles/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Новости", 
+		"/content/news/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Видео и аудио", 
+		"/content/media/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ urlrewrite.php
@@ -0,0 +1,17 @@
+<?
+$arUrlRewrite = array(
+	array(
+		"CONDITION" => "#^/content/articles/#",
+		"RULE" => "",
+		"ID" => "bitrix:news",
+		"PATH" => "/content/articles/index.php",
+	),
+	array(
+		"CONDITION" => "#^/content/news/#",
+		"RULE" => "",
+		"ID" => "bitrix:news",
+		"PATH" => "/content/news/index.php",
+	),
+);
+
+?>
